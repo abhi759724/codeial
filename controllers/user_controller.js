90cadd4a36c032d846f2bc8887d11dae5c74adda
Abhishek Pandey <123861746+abhi759724@users.noreply.github.com>
2023-10-07 02:02:26 +0530

    profile pictures

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,4 +1,6 @@
 const User = require("../models/user");
+const fs = require("file-system");
+const path = require("path");
 
 module.exports.userProfile = async (req, res) => {
   try {
@@ -24,6 +26,9 @@ module.exports.update = async (req, res) => {
         user.name = req.body.name;
         user.email = req.body.email;
         if (req.file) {
+          if (user.avatar) {
+            fs.unlinkSync(path.join(__dirname, "..", user.avatar));
+          }
           // saving the path of the uploaded file into the avatar field in the user
           user.avatar = User.avatarPath + "/" + req.file.filename;
         }
